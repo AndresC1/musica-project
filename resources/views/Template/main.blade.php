e5ee4afcffa3b322dc3818932c588501c792d23d
AndresC1 <andrescornejolira@gmail.com>
2023-01-09 16:37:30 -0600

    Creacion de plantilla principal

---
+++ resources/views/Template/main.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src={{ asset('js/Tailwind_conf.js') }}></script>
+    <title>Musica - ACL</title>
+</head>
+<body>
+    <h1 class="text-white bg-clifford">Musica - ACL</h1>
+    @yield('contenido')
+</body>
+</html>
