02cd2f2a27b162b1aaa0bc127f26881be311edf9
AndresC1 <andrescornejolira@gmail.com>
2023-01-10 13:14:43 -0600

    Seeder de generos

--- database/seeders/GeneroSeeder.php
+++ database/seeders/GeneroSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use DateTime;
 
 class GeneroSeeder extends Seeder
 {
@@ -14,6 +16,28 @@ class GeneroSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        DB::table('generos')->truncate();
+        DB::table('generos')->insert([
+            [
+                'nombre' => 'Example_1',
+                'imagen' => 'Example_1.jpg',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+            ],
+            [
+                'nombre' => 'Example_2',
+                'imagen' => 'Example_2.jpg',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+            ],
+            [
+                'nombre' => 'Example_3',
+                'imagen' => 'Example_3.jpg',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+            ]
+        ]);
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
