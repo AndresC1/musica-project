64aa98f9c5a9e0b12b1905be4aee5d3a15aef92a
AndresC1 <andrescornejolira@gmail.com>
2023-01-09 18:20:59 -0600

    Creacion del modo oscuro

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,14 @@
 'use strict'
+
+const mode = document.querySelector('#btnMode')
+const mainHTML = document.querySelector('html')
+
+mode.addEventListener('click', function() {
+    if(mainHTML.className === 'dark'){
+        mainHTML.className = ''
+        mode.className = mode.className + ' justify-end';
+    } else{
+        mainHTML.className = 'dark'
+        mode.className = 'bg-white w-14 h-auto rounded-full mx-3 border-[1px] mx-3 bg-white border-[#30343F] dark:bg-[#30343F] flex transition-all justify-start';
+    }
+})
--- resources/views/Template/main.blade.php
+++ resources/views/Template/main.blade.php
@@ -8,8 +8,22 @@
     <script src={{ asset('js/Tailwind_conf.js') }}></script>
     <title>Musica - ACL</title>
 </head>
-<body>
-    <h1 class="text-white bg-clifford">Musica - ACL</h1>
+<body class="bg-white dark:bg-[#121212] dark:text-[#eeeeee] transition-all">
+    <nav class="dark:bg-black bg-white w-2/12 min-h-screen h-auto dark:text-[#a7a7a7] border-r-[1px] dark:border-none">
+        <div class="flex justify-center items-center my-4">
+            <img class="w-4 h-auto" src={{ asset('storage/svg/Oscuro.svg') }} alt="Modo Oscuro">
+            <button id="btnMode" class="bg-white w-14 h-auto rounded-full border-[1px] mx-3 bg-white border-gray-500 dark:bg-[#30343F] flex transition-all justify-end">
+                <div class="dark:bg-[#121212] bg-green-600 rounded-full w-5 h-5 m-1 transition-all"></div>
+            </button>
+            <img class="w-5 h-auto" src={{ asset('storage/svg/Claro.svg') }} alt="Modo Claro">
+        </div>
+        <ul class="flex flex-col justify-center items-center mt-10">
+            <a href="http://" class="w-full h-14 flex justify-center items-center hover:font-semibold transition-all dark:hover:text-white">Canciones</a>
+            <a href="http://" class="w-full h-14 flex justify-center items-center hover:font-semibold transition-all dark:hover:text-white">Albumes</a>
+            <a href="http://" class="w-full h-14 flex justify-center items-center hover:font-semibold transition-all dark:hover:text-white">Artistas</a>
+        </ul>
+    </nav>
     @yield('contenido')
+    <script src={{ asset('js/app.js') }}></script>
 </body>
 </html>
