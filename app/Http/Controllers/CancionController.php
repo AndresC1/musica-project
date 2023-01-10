6bb50696781f787db014e608c2305669a672f105
AndresC1 <andrescornejolira@gmail.com>
2023-01-10 12:10:12 -0600

    Modelo, seeder, migracion y controller de Cancion

---
+++ app/Http/Controllers/CancionController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cancion;
+use App\Http\Requests\StoreCancionRequest;
+use App\Http\Requests\UpdateCancionRequest;
+
+class CancionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreCancionRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCancionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Cancion  $cancion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cancion $cancion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Cancion  $cancion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cancion $cancion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateCancionRequest  $request
+     * @param  \App\Models\Cancion  $cancion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCancionRequest $request, Cancion $cancion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Cancion  $cancion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cancion $cancion)
+    {
+        //
+    }
+}
