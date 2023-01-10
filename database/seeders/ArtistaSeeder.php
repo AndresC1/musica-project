65d0d732581dbf589382419aaa8498f0b9459750
AndresC1 <andrescornejolira@gmail.com>
2023-01-10 13:16:58 -0600

    Seeder de artista

--- database/seeders/ArtistaSeeder.php
+++ database/seeders/ArtistaSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use DateTime;
 
 class ArtistaSeeder extends Seeder
 {
@@ -14,6 +16,28 @@ class ArtistaSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        DB::table('artistas')->truncate();
+        DB::table('artistas')->insert([
+            [
+                'nombre' => 'Artist_1',
+                'imagen' => 'Artist_1.jpg',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+            ],
+            [
+                'nombre' => 'Artist_2',
+                'imagen' => 'Artist_2.jpg',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+            ],
+            [
+                'nombre' => 'Artist_3',
+                'imagen' => 'Artist_3.jpg',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+            ]
+        ]);
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
