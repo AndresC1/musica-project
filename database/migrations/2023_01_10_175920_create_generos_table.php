447890f2e54333b61f8348097858959fa9d4b8be
AndresC1 <andrescornejolira@gmail.com>
2023-01-10 12:17:25 -0600

    migracion de los generos

--- database/migrations/2023_01_10_175920_create_generos_table.php
+++ database/migrations/2023_01_10_175920_create_generos_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('generos', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
+            $table->string('imagen');
             $table->timestamps();
         });
     }
