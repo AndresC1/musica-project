7a258b715b6839ed0cb191ae0e65d55ec32df669
AndresC1 <andrescornejolira@gmail.com>
2023-01-09 18:22:28 -0600

    ajustes de diseño

--- resources/views/Template/main.blade.php
+++ resources/views/Template/main.blade.php
@@ -9,8 +9,8 @@
     <title>Musica - ACL</title>
 </head>
 <body class="bg-white dark:bg-[#121212] dark:text-[#eeeeee] transition-all">
-    <nav class="dark:bg-black bg-white w-2/12 min-h-screen h-auto dark:text-[#a7a7a7] border-r-[1px] dark:border-none">
-        <div class="flex justify-center items-center my-4">
+    <nav class="dark:bg-black bg-white w-2/12 min-h-screen pt-4 h-auto dark:text-[#a7a7a7] border-r-[1px] dark:border-none">
+        <div class="flex justify-center items-center mb-4">
             <img class="w-4 h-auto" src={{ asset('storage/svg/Oscuro.svg') }} alt="Modo Oscuro">
             <button id="btnMode" class="bg-white w-14 h-auto rounded-full border-[1px] mx-3 bg-white border-gray-500 dark:bg-[#30343F] flex transition-all justify-end">
                 <div class="dark:bg-[#121212] bg-green-600 rounded-full w-5 h-5 m-1 transition-all"></div>
