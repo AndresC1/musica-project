b8105a6829a11a11761ec48f1012e90d5045930e
AndresC1 <andrescornejolira@gmail.com>
2023-01-09 18:35:57 -0600

    anexo de los colores a tailwind

--- public/js/Tailwind_conf.js
+++ public/js/Tailwind_conf.js
@@ -3,7 +3,9 @@ tailwind.config = {
     theme: {
     extend: {
         colors: {
-        clifford: '#da373d',
+        color_oscuro_1: '#121212',
+        color_blanco_1: '#eeeeee',
+        color_blanco_2: '#a7a7a7',
         }
     }
     }
