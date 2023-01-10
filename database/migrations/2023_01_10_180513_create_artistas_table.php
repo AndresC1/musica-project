a6ae9adfd60379bce1433dbead3ae3d491656b16
AndresC1 <andrescornejolira@gmail.com>
2023-01-10 13:03:52 -0600

    migracion de artistas

--- database/migrations/2023_01_10_180513_create_artistas_table.php
+++ database/migrations/2023_01_10_180513_create_artistas_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('artistas', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
+            $table->string('imagen');
             $table->timestamps();
         });
     }
