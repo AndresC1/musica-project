5586ee9f9ac5a8de980146d7bf6c88dbaddbf49b
AndresC1 <andrescornejolira@gmail.com>
2023-01-10 13:25:38 -0600

    Seeder de canciones

--- database/seeders/CancionSeeder.php
+++ database/seeders/CancionSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use DateTime;
 
 class CancionSeeder extends Seeder
 {
@@ -14,6 +16,46 @@ class CancionSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        DB::table('cancions')->truncate();
+        DB::table('cancions')->insert([
+            [
+                'nombre' => 'Example_song_1',
+                'imagen' => 'Example_song_1.jpg',
+                'archCancion' => 'Song_1.mp3',
+                'color' => '#efefef',
+                'anio' => '2022',
+                'IdGenero' => 1,
+                'IdAlbum' => 1,
+                'IdArtistas' => 1,
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+            ],
+            [
+                'nombre' => 'Example_song_2',
+                'imagen' => 'Example_song_2.jpg',
+                'archCancion' => 'Song_2.mp3',
+                'color' => '#efefef',
+                'anio' => '2022',
+                'IdGenero' => 2,
+                'IdAlbum' => 2,
+                'IdArtistas' => 2,
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+            ],
+            [
+                'nombre' => 'Example_song_3',
+                'imagen' => 'Example_song_3.jpg',
+                'archCancion' => 'Song_3.mp3',
+                'color' => '#efefef',
+                'anio' => '2022',
+                'IdGenero' => 3,
+                'IdAlbum' => 3,
+                'IdArtistas' => 3,
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+            ]
+        ]);
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
