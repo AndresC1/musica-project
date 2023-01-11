b1b336c0bd1ee1f9ff132fb7cffffae50b90bfdb
AndresC1 <andrescornejolira@gmail.com>
2023-01-10 21:12:55 -0600

    Salida de datos de las canciones

--- app/Http/Controllers/CancionController.php
+++ app/Http/Controllers/CancionController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Cancion;
 use App\Http\Requests\StoreCancionRequest;
 use App\Http\Requests\UpdateCancionRequest;
+use App\Models\Artista;
+use Exception;
+use stdClass;
 
 class CancionController extends Controller
 {
@@ -82,5 +85,44 @@ class CancionController extends Controller
     public function destroy(Cancion $cancion)
     {
         //
+    }
+    public function IndexAPI(){
+        try {
+            $canciones = array();
+            foreach(Cancion::all() as $cancion){
+                $ObjCancion = new stdClass;
+                $ObjCancion->id = $cancion->id;
+                $ObjCancion->nombre = $cancion->nombre;
+                $ObjCancion->imagen = $cancion->imagen;
+                $ObjCancion->archCancion = $cancion->archCancion;
+                $ObjCancion->color = $cancion->color;
+                $ObjCancion->anio = $cancion->anio;
+                $ObjCancion->genero = $this->LimData($cancion->genero);
+                $ObjCancion->album = $this->LimData($cancion->album);
+                $ObjCancion->artistas = $this->MuestraArtistas($cancion->artistas);
+                array_push($canciones, $ObjCancion);
+            }
+            return response($canciones, 200);
+        } catch (Exception $e) {
+            return response(['A ocurrido un error', $e->getMessage()], 400);
+        }
+    }
+    function LimData($ListaDatos){
+        $element = new stdClass;
+        $element->id = $ListaDatos->id;
+        $element->nombre = $ListaDatos->nombre;
+        $element->imagen = $ListaDatos->imagen;
+        return $element;
+    }
+    function MuestraArtistas($datos){
+        $salida = array();
+        Foreach($datos as $dat){
+            $valores = Artista::find($dat->IdArtistas);
+            $val = new stdClass;
+            $val->nombre = $valores->nombre;
+            $val->imagen = $valores->imagen;
+            array_push($salida, $val);
+        }
+        return $salida;
     }
 }
