7fe5dce3b5e74f45ea37b03a0ce1da5c42e4b1bd
AndresC1 <andrescornejolira@gmail.com>
2023-01-11 02:09:14 -0600

    Creacion de las rutas para la muestra de un dato

--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,8 @@ Route::get('/v1/generos', [GeneroController::class, 'IndexAPI']);
 Route::get('/v1/canciones', [CancionController::class, 'IndexAPI']);
 Route::get('/v1/albumes', [AlbumController::class, 'IndexAPI']);
 Route::get('/v1/artistas', [ArtistaController::class, 'IndexAPI']);
+
+Route::get('/v1/album/{album}', [AlbumController::class, 'ShowAPI']);
+Route::get('/v1/artista/{artista}', [ArtistaController::class, 'ShowAPI']);
+Route::get('/v1/cancion/{cancion}', [CancionController::class, 'ShowAPI']);
+Route::get('/v1/genero/{genero}', [GeneroController::class, 'ShowAPI']);
