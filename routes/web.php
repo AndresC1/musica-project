f450657ddd0a9d007fa3627a613f696715bb7e2a
AndresC1 <andrescornejolira@gmail.com>
2023-01-11 13:49:18 -0600

    Rutas de la muestra de los datos genero y album

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\AlbumController;
+use App\Http\Controllers\GeneroController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('Template.main');
 });
+
+Route::get('/generos', [GeneroController::class, 'index'])->name('ListGeneros');
+Route::get('/albumes', [AlbumController::class, 'index'])->name('ListAlbumes');
