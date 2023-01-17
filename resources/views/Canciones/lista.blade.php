2bceed9d3857614d95dce3a0d65aa1e1819c4e71
AndresC1 <andrescornejolira@gmail.com>
2023-01-17 11:50:29 -0600

    Anexo del boton de agregar cancion

--- resources/views/Canciones/lista.blade.php
+++ resources/views/Canciones/lista.blade.php
@@ -2,7 +2,7 @@
 
 @section('contenido')
     <h1 class="w-full h-20 text-2xl flex justify-center items-center text-white font-bold mb-4">Canciones</h1>
-    <div id="contenedor" class="w-full m-auto h-auto max-h-[75vh] flex flex-wrap lg:justify-start justify-center overflow-y-auto">
+    <div id="contenedor" class="w-full m-auto h-auto max-h-[70vh] flex flex-wrap lg:justify-start justify-center overflow-y-auto">
         @foreach ($Canciones as $cancion)
             <a href="" class="lg:w-5/12 xl:max-w-[25em] max-w-[30em] w-full h-20 bg-[#222] hover:bg-[#333] m-2 rounded-lg flex flex-row justify-start items-center group transition-all bg-gradient-to-r hover:from-[#333] from-[#222] to-[#222] hover:to-[{{$cancion->color}}]">
                 <img class="w-14 h-14 m-3 rounded" src={{ asset('storage/img/Albumes/'.$cancion->album->imagen) }} alt={{$cancion->nombre}}>
@@ -10,4 +10,8 @@
             </a>
         @endforeach
     </div>
+    <button class="lg:w-auto lg:px-4 w-16 h-16 rounded-full fixed lg:bottom-14 bottom-5 right-5 lg:right-16 flex justify-center items-center hover:scale-110 bg-white">
+        <h1 class="lg:block hidden">Agregar cancion</h1>
+        <img src={{asset('storage/svg/add.svg')}} class="w-7 h-auto lg:ml-2" alt="">
+    </button>
 @endsection
