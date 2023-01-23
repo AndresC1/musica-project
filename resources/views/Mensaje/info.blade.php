4d3f57817dfbd0daeeb4cc97c0495b12a61339b2
AndresC1 <andrescornejolira@gmail.com>
2023-01-23 13:27:00 -0600

    Vista de muestra de informacion

---
+++ resources/views/Mensaje/info.blade.php
@@ -0,0 +1,20 @@
+@extends('Template.form')
+
+@section('contenido')
+    <div class="w-full min-h-[20rem] h-[100vh] flex flex-col justify-center items-center">
+        <h1 class="w-11/12 max-w-[30rem] rounded-lg h-20 bg-[#333] text-green-400 font-semibold flex justify-center items-center p-4 grid grid-cols-6">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-auto w-7 text-green-400 mr-1 col-span-1">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" /></svg> 
+            <b class="col-span-5">
+                {{$informacion}}
+            </b>   
+        </h1>
+        <a href={{ route('inicio') }} class="my-5 border-2 hover:bg-red-700 hover:text-white border-red-700 text-red-700 p-3 rounded-lg font-semibold flex justify-center items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-auto w-6 mr-1">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+            </svg>              
+            Ir al inicio</a>
+    </div>
+@endsection
+    {{-- <h1 class="text-white"></h1> --}}
+    {{-- <img class="h-auto w-7 text-green-400 mr-3" src={{asset('storage/svg/check.svg')}} alt="check"> --}}
