dbdc5966d26a60dd7bc3c95aaca990277fdfdd71
AndresC1 <andrescornejolira@gmail.com>
2023-01-24 17:23:50 -0600

    Anexo de favicon

--- resources/views/Template/main.blade.php
+++ resources/views/Template/main.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href={{asset('storage/favicon.ico')}} type="image/x-icon">
     <script src="https://cdn.tailwindcss.com"></script>
     <script src={{ asset('js/Tailwind_conf.js') }}></script>
     <link rel="stylesheet" href={{ asset('css/style.css')}}>
