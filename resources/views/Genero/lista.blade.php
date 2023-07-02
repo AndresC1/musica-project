b238d5551184355308570f5d4d0c5ecb3148416e
AndresC1 <andrescornejolira@gmail.com>
2023-07-01 19:19:14 -0600

    cambio en el conteo de la lista de generos

--- resources/views/Genero/lista.blade.php
+++ resources/views/Genero/lista.blade.php
@@ -3,8 +3,8 @@
 @section('contenido')
     <h1 class="w-full h-20 text-2xl flex justify-center items-center text-white font-bold mb-4">Generos</h1>
     <div id="contenedor" class="w-full m-auto h-auto max-h-[75vh] flex flex-row flex-wrap justify-center sm:justify-start overflow-y-auto">
-        @if (count($Generos)!=0)
-            @foreach ($Generos as $genero)
+        @if ($Generos->count()!=0)
+        @foreach ($Generos as $genero)
                 <div class="sm:w-52 w-72 h-72 bg-[#222] hover:bg-[#333] m-2 text-gray-400 text-lg rounded-xl group lg:hover:scale-105 transition-all">
                     <img class="rounded-md transition-all w-40 h-40 mt-10 mb-6 mx-auto sm:m-6" src={{ asset('storage/img/Generos/'.$genero->imagen) }} alt="generos">
                     <h3 class="w-auto min-h-8 max-h-14 flex mx-2 justify-center items-center font-medium group-hover:text-color_blanco_1 transition-all">{{$genero->nombre}}</h3>
