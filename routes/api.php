25e0402e673d96dbb45afbdb3ff06c8231214d3b
AndresC1 <andrescornejolira@gmail.com>
2023-07-04 13:17:26 -0600

    Bloqueo de rutas con sanctum para auth

--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,26 @@ use App\Http\Controllers\AlbumController;
 use App\Http\Controllers\ArtistaController;
 use App\Http\Controllers\CancionController;
 use App\Http\Controllers\GeneroController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/v1/generos', [GeneroController::class, 'IndexAPI']);
-Route::get('/v1/canciones', [CancionController::class, 'IndexAPI']);
-Route::get('/v1/albumes', [AlbumController::class, 'IndexAPI']);
-Route::get('/v1/artistas', [ArtistaController::class, 'IndexAPI']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/v1/album/{album}', [AlbumController::class, 'ShowAPI']);
-Route::get('/v1/artista/{artista}', [ArtistaController::class, 'ShowAPI']);
-Route::get('/v1/cancion/{cancion}', [CancionController::class, 'ShowAPI']);
-Route::get('/v1/genero/{genero}', [GeneroController::class, 'ShowAPI']);
+Route::middleware('auth:sanctum')->group(function () {
+    // Auth
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'user']);
+    // Genero
+    Route::get('/generos', [GeneroController::class, 'IndexAPI']);
+    Route::get('/genero/{genero}', [GeneroController::class, 'ShowAPI']);
+    // Cancion
+    Route::get('/canciones', [CancionController::class, 'IndexAPI']);
+    Route::get('/cancion/{cancion}', [CancionController::class, 'ShowAPI']);
+    // Album
+    Route::get('/albumes', [AlbumController::class, 'IndexAPI']);
+    Route::get('/album/{album}', [AlbumController::class, 'ShowAPI']);
+    // Artista
+    Route::get('/artistas', [ArtistaController::class, 'IndexAPI']);
+    Route::get('/artista/{artista}', [ArtistaController::class, 'ShowAPI']);
+});
